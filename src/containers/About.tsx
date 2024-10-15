5cca04b0951da902dc173531c5670a91cd94ab09
erne.undze@gmail.com <erne.undze@gmail.com>
2024-10-15 15:21:32 +0300

    another fix on mobile

--- src/containers/About.tsx
+++ src/containers/About.tsx
@@ -2,12 +2,12 @@
 const About = () => {
   return (
     <>
-    <div className='bg-footerColor px-32 mx-auto justify-center items-center flex flex-col  text-textColor  text-center font-poppins'>
+    <div className='bg-footerColor px-10 md:px-32 mx-auto justify-center items-center flex flex-col  text-textColor  text-center font-poppins'>
         <div className='py-10 max-w-6xl'>
             <h1 className='text-3xl italic sm:text-5xl'>Apie mus</h1>
         </div>
 
-        <div className='pb-12 max-w-6xl'>
+        <div className='pb-12 max-w-screen md:max-w-6xl'>
             <p className='text-xl sm:text-2xl'>Kiekvienas mūsų sukurtas patiekalas pagaminamas kokybiškai ir iš širdies, siekiant ne tik išskirtinio skonio, bet ir estetinės išraiškos, kad kiekvienas įvykis taptų ypatingu, paliekant neišdildomą įspūdį.</p>
         </div>
     </div>
--- src/containers/Photos.tsx
+++ src/containers/Photos.tsx
@@ -6,7 +6,7 @@ const Photos = () => {
         <div className='py-10 px-32 max-w-6xl '>
             <h1 className='text-3xl italic sm:text-5xl'>Galerija</h1>
         </div>
-        <div className="mx-auto grid w-screen px-6 md:px-16 grid-cols-2 gap-x-6 gap-y-6 sm:gap-y-10 lg:mx-0 lg:max-w-none lg:grid-cols-3 pt-6">
+        <div className="mx-auto grid w-screen px-6 md:px-16 grid-cols-2 gap-x-3 gap-y-3 md:gap-y-6 md:gap-x-6 sm:gap-y-10 lg:mx-0 lg:max-w-none lg:grid-cols-3 pt-6">
           <PhotoDiv></PhotoDiv>
         </div>
     </div>
